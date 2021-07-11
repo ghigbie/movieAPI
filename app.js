76af3355e736f01fcad5173597fc214dbea53dfc
ghigbie <gehigbie@gmail.com>
2021-07-11 10:28:37 -0400

    Update router

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
 
+// /now_playing
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+// /movies/...
+const movieRouter = require('./routes/movie');
+// /serach/...
+const serachRouter = require('./routes/serach')
+
 
 const app = express();
 
@@ -22,7 +27,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/movies', movieRouter);
+app.use('/search', searchRouter);
+
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
