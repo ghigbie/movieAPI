3e6c98d86a944f132fa824e8b74d09c7f3885a88
ghigbie <gehigbie@gmail.com>
2021-07-14 21:01:57 -0400

    Add filter and find responses

--- routes/movie.js
+++ routes/movie.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const movieDetails = require('./../data/movieDetails');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.json({ title: 'Movie' });
+  const id = 56292; 
+  const foundMovie = movieDetails.find( movie => movie.id === id);
+  res.json({ 
+    title: 'Movie',
+    data: foundMovie,
+   });
 });
 
 module.exports = router;
--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const movieData = require('./../data/movies');
+
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.json({ title: 'Search' });
+  const searchTerm = 'Toy'
+  const filteredMovies = movieData.map( movie => {
+   return movie.title.toLowerCase().includes(searchTerm.toLowerCase()) ? movie : null;
+  });
+
+  res.json({ 
+    title: 'Search',
+    data: filteredMovies,
+  });
 });
 
 module.exports = router;
