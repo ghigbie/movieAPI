b5f663736f2337f68bf0bf328539d6aaa19f1873
ghigbie <gehigbie@gmail.com>
2021-07-20 19:07:37 -0400

    Rework search routes logic

--- routes/search.js
+++ routes/search.js
@@ -4,22 +4,28 @@ const movieData = require('./../data/movies');
 
 const requireSearchQuery = (req, res, next) => {
   const searchTerm = req.query.query;
+  const results = movieData.filter(movie => {
+    let found = movie.overview.inlcudes(searchTerm)
+  })
   !searchTerm && res.json({ message: 'Query is required'});
   next();
+}
+
+const searchMovieTerms = (searchTerm) => {
+  const notFoundMessage = 'A movie matching this criteria could not be found';
+  return movieData.filter(movie => movie.overview.inlcudes(searchTerm) || movie.title.includes(searchTerm) || notFoundMessage);
 }
 
 router.use(requireSearchQuery); //will be used by all routes in this router
 
 router.get('/movie', (req, res, next) => {
-  res.json({
-    tes: "test movie response"
-  })
+  const results = searchMovieTerms(req.query.query);
+  res.json({ results });
 });
 
 router.get('/person', (req, res, next) => {
-  res.json({
-    tes: "test person response"
-  })
+  const results = searchMovieTerms(req.query.query);
+  res.json({ results });
 });
 
 
